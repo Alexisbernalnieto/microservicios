7aae19223967b32f67e2c0c6ce38a50e33bafe5b
mariana <alexisbernalnieto119@gmail.com>
2024-10-26 13:08:42 -0600

    Cambios en server de api gateway

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,12 +1,15 @@
-const gateway = require('fast-gateway');
 const express = require('express');
 const path = require('path');
+const gateway = require('fast-gateway');
 
 // Utiliza el puerto proporcionado por Render o el puerto 3000 si no está definido
 const port = process.env.PORT || 3000;
 
-// Crear el servidor API Gateway
-const server = gateway({
+// Crear el servidor Express principal
+const app = express();
+
+// Configurar Fast-Gateway como middleware en el servidor Express principal
+const gatewayServer = gateway({
     routes: [
         {
             prefix: '/pagos',
@@ -26,20 +29,15 @@ const server = gateway({
     ]
 });
 
-// Iniciar el API Gateway en el puerto proporcionado por Render o el puerto por defecto
-server.start(port).then(() => {
-    console.log('API Gateway ejecutándose en el puerto: ' + port);
-});
+// Montar el gateway como middleware en Express
+app.use('/api', gatewayServer.middleware());
 
-// Crear el servidor Express para manejar las solicitudes del contenido estático
-const app = express();
-
-// Servir el archivo index.html desde la raíz del directorio api-gateway
+// Servir el archivo index.html para la raíz
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Iniciar el servidor Express en el mismo puerto proporcionado por Render
+// Iniciar el servidor Express que incluye el API Gateway y el contenido estático
 app.listen(port, () => {
-    console.log('Servidor de contenido estático ejecutándose en el puerto: ' + port);
+    console.log(`Servidor de contenido estático y API Gateway ejecutándose en el puerto: ${port}`);
 });
